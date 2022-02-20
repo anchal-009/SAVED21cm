9ea811702832858b2887770bc614f63fd926c200
Anchal Saxena <anchal.saxena.009@gmail.com>
2022-02-20 17:53:08 +0100

    Removed files from previous version

--- tests/test_02.py
+++
@@ -1,8 +0,0 @@
-import sys; sys.path.insert(1, './../')
-from src.runpipe import Pipeline
-import settings as set
-
-set.checkSettings()
-pipe = Pipeline(nu=set.nu, nLST=set.LST, ant=set.ANT, path21TS=set.path21TS,
-                pathFgTS=set.pathFgTS, indexFg=0, index21=1000)
-pipe.runPipeline()
